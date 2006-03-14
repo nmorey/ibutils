743783b6789ae78af716779265d829372e4316a7
Eitan Zahavi <eitan@mellanox.co.il>
2006-03-14 15:50:26 +0000

    r5816: When initalizing linkSpeedActive with value of 5 or greater, used the full linkSpeedEnabled instead of the actuall speed.

--- ibmgtsim/src/sma.cpp
+++ ibmgtsim/src/sma.cpp
@@ -367,7 +367,7 @@ void IBMSSma::initPortInfo()
       ib_port_info_set_port_state( &tmpPortInfo, IB_LINK_INIT);
       ib_port_info_set_link_speed_sup( linkSpeed, &tmpPortInfo);
       // LinkSpeedEnabled and LinkSpeedActive
-      tmpPortInfo.link_speed = linkSpeed | (linkSpeed << 4);
+      tmpPortInfo.link_speed = linkSpeed | (pNodePortData->speed << 4);
            
     }
        
