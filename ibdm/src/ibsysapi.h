8aa447936597d96071157cd7fa05b8f04ff8e67e
Aviad Yehezkel <aviadye@mellanox.co.il>
2010-04-27 11:18:50 +0300

    Fixed indentation in libsysapi.
    Signed-off-by: Aviad Yehezkel <aviadye@mellanox.co.il>
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>

--- ibdm/src/ibsysapi.h
+++ ibdm/src/ibsysapi.h
@@ -40,14 +40,16 @@ extern "C" {
 
 /* verbose mask */
 enum ibSysVerboseLevel {
-  IBSYS_QUITE = 0,
-  IBSYS_ERROR = 1,
-  IBSYS_WARN  = 2,
-  IBSYS_INFO  = 4,
-  IBSYS_DEBUG = 8
+    IBSYS_QUITE = 0,
+    IBSYS_ERROR = 1,
+    IBSYS_WARN  = 2,
+    IBSYS_INFO  = 4,
+    IBSYS_DEBUG = 8
 };
 
+
 extern int ibSysVerbose;
+
 
 /* read the IBNL with specific configuration and prepare the local fabric */
 int ibSysInit(char *sysType, char *cfg);
@@ -56,25 +58,26 @@ int ibSysInit(char *sysType, char *cfg);
 /* return the number of filled paths in numPaths */
 /* end of DR is a -1 */
 int ibSysGetDrPathsToNode(char *fromNode, char *toNode,
-				   int *numPaths, int**drPaths);
+        int *numPaths, int**drPaths);
 
 /* query all node names in the system */
 int ibSysGetNodes(int *numNodes, const char **nodeNames);
 
 /* query name of front pannel port of a node port */
 int ibSysGetNodePortSysPort(char *nodeName, int portNum,
-				    const char **sysPortName);
+        const char **sysPortName);
 
 /* query node name and port given front pannel port name */
 int ibSysGetNodePortOnSysPort(char *sysPort,
-					const char**nodeName, int *portNum);
+        const char**nodeName, int *portNum);
 
 /* query other side of the port node port */
 int ibSysGetRemoteNodePort(char *nodeName, int portNum,
-				   const char **remNode, int *remPortNum);
+        const char **remNode, int *remPortNum);
 
 #ifdef __cplusplus
-} /* extern "C" */
-#endif
+}                   /* extern "C" */
+#endif          /* ifdef __cplusplus */
+
 
 #endif /* IBDM_SYS_API_H */
