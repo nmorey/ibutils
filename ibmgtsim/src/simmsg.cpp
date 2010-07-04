b84de3b2215a9392e453c3e532b81f8e84e19520
Aviad Yehezkel <aviadye@mellanox.co.il>
2010-07-04 11:37:53 +0300

    Fixed indentation in ibmgtsim/src/simmsg.h + ibmgtsim/src/simmsg.cpp files.
    Signed-off-by: Aviad Yehezkel <aviadye@mellanox.co.il>
    Signed-off-by: Yevgeny Kliteynik <kliteyn@dev.mellanox.co.il>

--- ibmgtsim/src/simmsg.cpp
+++ ibmgtsim/src/simmsg.cpp
@@ -43,6 +43,7 @@
 #include "simmsg.h"
 #include <inttypes.h>
 
+
 #ifndef PRIx64
 # if __WORDSIZE == 64
 #  define __PRI64_PREFIX   "l"
@@ -54,134 +55,150 @@
 # define PRIx64 __PRI64_PREFIX "x"
 #endif
 
-std::string
-__ibms_dump_conn_msg(
-  IN const ibms_client_msg_t *p_msg)
-{
-  char buff[32];
-  char msg[512];
-  strncpy(buff,p_msg->msg.conn.host,31);
-  buff[31] = '\0';
 
-  sprintf(msg,"MSG: CONN"
-          " Port:%u Guid:0x%016" PRIx64 " Host:%s InPort:%u",
-          p_msg->msg.conn.port_num,
-          p_msg->msg.conn.port_guid,
-          p_msg->msg.conn.host,
-          p_msg->msg.conn.in_msg_port
-          );
-  return msg;
+std::string
+__ibms_dump_conn_msg(IN const ibms_client_msg_t *p_msg)
+{
+    char buff[32];
+    char msg[512];
+    strncpy(buff,p_msg->msg.conn.host,31);
+    buff[31] = '\0';
+
+    sprintf(msg,"MSG: CONN"
+            " Port:%u Guid:0x%016" PRIx64 " Host:%s InPort:%u",
+            p_msg->msg.conn.port_num,
+            p_msg->msg.conn.port_guid,
+            p_msg->msg.conn.host,
+            p_msg->msg.conn.in_msg_port);
+    return msg;
 }
 
-std::string
-__ibms_dump_disconn_msg(
-  IN const ibms_client_msg_t *p_msg)
-{
-  char buff[32];
-  char msg[512];
-  strncpy(buff,p_msg->msg.conn.host,31);
-  buff[31] = '\0';
 
-  sprintf(msg,"MSG: DISCONNECT "
-          " Port:%u Guid:0x%016" PRIx64 "",
-          p_msg->msg.disc.port_num,
-          p_msg->msg.disc.port_guid
-          );
-  return msg;
+std::string
+__ibms_dump_disconn_msg(IN const ibms_client_msg_t *p_msg)
+{
+    char buff[32];
+    char msg[512];
+    strncpy(buff,p_msg->msg.conn.host,31);
+    buff[31] = '\0';
+
+    sprintf(msg,"MSG: DISCONNECT "
+            " Port:%u Guid:0x%016" PRIx64 "",
+            p_msg->msg.disc.port_num,
+            p_msg->msg.disc.port_guid);
+    return msg;
 }
 
-std::string
-__ibms_dump_cap_msg(
-  IN const ibms_client_msg_t *p_msg)
-{
-  char msg[512];
-  sprintf(msg, "MSG: CAP");
-  return msg;
-}
 
 std::string
-__ibms_dump_bind_msg(
-  IN const ibms_client_msg_t *p_msg)
+__ibms_dump_cap_msg(IN const ibms_client_msg_t *p_msg)
 {
-  char msg[512];
-  sprintf(msg, "MSG: BIND");
-  if (p_msg->msg.bind.mask & IBMS_BIND_MASK_PORT)
-    sprintf(msg,"%s Port:%u ",msg, p_msg->msg.bind.port);
-  if (p_msg->msg.bind.mask & IBMS_BIND_MASK_QP)
-    sprintf(msg, "%s QP:%u ", msg, p_msg->msg.bind.qpn);
-  if (p_msg->msg.bind.mask & IBMS_BIND_MASK_CLASS)
-    sprintf(msg, "%s Class:0x%X ", msg, p_msg->msg.bind.mgt_class);
-  if (p_msg->msg.bind.mask & IBMS_BIND_MASK_METH)
-    sprintf(msg, "%s Method:0x%X ", msg, p_msg->msg.bind.method);
-  if (p_msg->msg.bind.mask & IBMS_BIND_MASK_ATTR)
-    sprintf(msg, "%s Attribute:0x%X ", msg, p_msg->msg.bind.attribute);
-  if (p_msg->msg.bind.mask & IBMS_BIND_MASK_INPUT) {
-    if (p_msg->msg.bind.only_input)
-      sprintf(msg, "%s Direction:IN",msg);
+    char msg[512];
+    sprintf(msg, "MSG: CAP");
+    return msg;
+}
+
+
+std::string
+__ibms_dump_bind_msg(IN const ibms_client_msg_t *p_msg)
+{
+    char msg[512];
+    sprintf(msg, "MSG: BIND");
+    if (p_msg->msg.bind.mask & IBMS_BIND_MASK_PORT)
+        sprintf(msg,"%s Port:%u ",msg, p_msg->msg.bind.port);
+    if (p_msg->msg.bind.mask & IBMS_BIND_MASK_QP)
+        sprintf(msg, "%s QP:%u ", msg, p_msg->msg.bind.qpn);
+    if (p_msg->msg.bind.mask & IBMS_BIND_MASK_CLASS)
+        sprintf(msg, "%s Class:0x%X ", msg, p_msg->msg.bind.mgt_class);
+    if (p_msg->msg.bind.mask & IBMS_BIND_MASK_METH)
+        sprintf(msg, "%s Method:0x%X ", msg, p_msg->msg.bind.method);
+    if (p_msg->msg.bind.mask & IBMS_BIND_MASK_ATTR)
+        sprintf(msg, "%s Attribute:0x%X ", msg, p_msg->msg.bind.attribute);
+    if (p_msg->msg.bind.mask & IBMS_BIND_MASK_INPUT) {
+        if (p_msg->msg.bind.only_input)
+            sprintf(msg, "%s Direction:IN",msg);
+        else
+            sprintf(msg, "%s Direction:IN/OUT", msg);
+    }
+    return msg;
+}
+
+
+std::string
+__ibms_dump_mad_msg(IN const ibms_client_msg_t *p_msg)
+{
+    char msg[1024];
+    sprintf(msg,
+            "MSG: MAD"
+            " ADDRESS:"
+            " SLID:0x%04X"
+            " DLID:0x%04X"
+            " SQPN:%u"
+            " DQPN:%u"
+            " PKEY:%u"
+            " SL:%u"
+            " CLASS:0x%02X"
+            " METHOD:0x%02X"
+            " STATUS:0x%04X"
+            " TID:0x%016"PRIx64"",
+            p_msg->msg.mad.addr.slid,
+            p_msg->msg.mad.addr.dlid,
+            p_msg->msg.mad.addr.sqpn,
+            p_msg->msg.mad.addr.dqpn,
+            p_msg->msg.mad.addr.pkey_index,
+            p_msg->msg.mad.addr.sl,
+            p_msg->msg.mad.header.mgmt_class,
+            p_msg->msg.mad.header.method,
+            p_msg->msg.mad.header.status,
+            p_msg->msg.mad.header.trans_id);
+    return msg;
+}
+
+
+std::string
+ibms_get_msg_str(IN const ibms_client_msg_t *p_msg)
+{
+    std::string msgStr;
+
+    switch (p_msg->msg_type) {
+    case IBMS_CLI_MSG_CONN:
+        msgStr = __ibms_dump_conn_msg(p_msg);
+        break;
+    case IBMS_CLI_MSG_DISCONN:
+        msgStr = __ibms_dump_disconn_msg(p_msg);
+        break;
+    case IBMS_CLI_MSG_BIND:
+        msgStr = __ibms_dump_bind_msg(p_msg);
+        break;
+    case IBMS_CLI_MSG_CAP:
+        msgStr = __ibms_dump_cap_msg(p_msg);
+        break;
+    case IBMS_CLI_MSG_MAD:
+        msgStr = __ibms_dump_mad_msg(p_msg);
+        break;
+    default:
+        msgStr = std::string("MSG: UNDEFINED");
+    }
+    return msgStr;
+}
+
+
+void
+ibms_dump_msg(IN const ibms_client_msg_t *p_msg)
+{
+    printf("%s", ibms_get_msg_str(p_msg).c_str());
+}
+
+
+const char *
+ibms_get_resp_str(IN const ibms_response_t *p_response)
+{
+    if (p_response->status)
+        return "REMOTE_ERROR";
     else
-      sprintf(msg, "%s Direction:IN/OUT", msg);
-  }
-  return msg;
+        return "SUCCESS";
 }
 
-std::string
-__ibms_dump_mad_msg(
-  IN const ibms_client_msg_t *p_msg)
-{
-  char msg[1024];
-  sprintf(msg,
-          "MSG: MAD"
-          " ADDRESS:"
-          " SLID:0x%04X"
-          " DLID:0x%04X"
-          " SQPN:%u"
-          " DQPN:%u"
-          " PKEY:%u"
-          " SL:%u"
-          " CLASS:0x%02X"
-          " METHOD:0x%02X"
-          " STATUS:0x%04X"
-          " TID:0x%016"PRIx64"",
-          p_msg->msg.mad.addr.slid,
-          p_msg->msg.mad.addr.dlid,
-          p_msg->msg.mad.addr.sqpn,
-          p_msg->msg.mad.addr.dqpn,
-          p_msg->msg.mad.addr.pkey_index,
-          p_msg->msg.mad.addr.sl,
-          p_msg->msg.mad.header.mgmt_class,
-          p_msg->msg.mad.header.method,
-          p_msg->msg.mad.header.status,
-          p_msg->msg.mad.header.trans_id);
-  return msg;
-}
-
-std::string
-ibms_get_msg_str(
-  IN const ibms_client_msg_t *p_msg)
-{
-  std::string msgStr;
-
-  switch (p_msg->msg_type) {
-  case IBMS_CLI_MSG_CONN:
-    msgStr = __ibms_dump_conn_msg(p_msg);
-    break;
-  case IBMS_CLI_MSG_DISCONN:
-    msgStr = __ibms_dump_disconn_msg(p_msg);
-    break;
-  case IBMS_CLI_MSG_BIND:
-    msgStr = __ibms_dump_bind_msg(p_msg);
-    break;
-  case IBMS_CLI_MSG_CAP:
-    msgStr = __ibms_dump_cap_msg(p_msg);
-    break;
-  case IBMS_CLI_MSG_MAD:
-    msgStr = __ibms_dump_mad_msg(p_msg);
-    break;
-  default:
-    msgStr = std::string("MSG: UNDEFINED");
-  }
-  return msgStr;
-}
 
 std::string
 ibms_get_mad_header_str(ib_mad_t madHeader)
@@ -212,6 +229,7 @@ ibms_get_mad_header_str(ib_mad_t madHeader)
     return (std::string)msg;
 };
 
+
 std::string
 ibms_get_node_info_str(ib_node_info_t*     pNodeInfo)
 {
@@ -228,6 +246,7 @@ ibms_get_node_info_str(ib_node_info_t*     pNodeInfo)
             ib_node_info_get_local_port_num(pNodeInfo));
     return (std::string)msg;
 }
+
 
 std::string
 ibms_get_port_info_str(ib_port_info_t*     pPortInfo)
@@ -253,19 +272,3 @@ ibms_get_port_info_str(ib_port_info_t*     pPortInfo)
     return (std::string)msg;
 };
 
-void
-ibms_dump_msg(
-  IN const ibms_client_msg_t *p_msg)
-{
-  printf("%s", ibms_get_msg_str(p_msg).c_str());
-}
-
-const char *
-ibms_get_resp_str(
-  IN const ibms_response_t *p_response)
-{
-  if (p_response->status)
-    return "REMOTE_ERROR";
-  else
-    return "SUCCESS";
-}
--- ibmgtsim/src/simmsg.h
+++ ibmgtsim/src/simmsg.h
@@ -33,28 +33,30 @@
 
 /****h* IBMgtSim/Messages
  * NAME
- *	 Messages
+ *  Messages
  *
  * DESCRIPTION
- * 	Declaration of messages between the client and simulator
+ *  Declaration of messages between the client and simulator
  *
  * Environment:
- * 	Linux User Mode
+ *  Linux User Mode
  *
  * $Revision: 1.7 $
  *
  * AUTHOR
- *	Eitan Zahavi, Mellanox
+ *  Eitan Zahavi, Mellanox
  *
  *********/
 
 #ifndef IBMGTSIM_CLIENT_IFC
 #define IBMGTSIM_CLIENT_IFC
 
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
 
 #include <iba/ib_types.h>
 #include "ib_types_extend.h"
@@ -68,15 +70,14 @@ extern "C"
 *
 * SYNOPSIS
 */
-typedef enum _ibms_cli_msg_type
-{
-  IBMS_CLI_MSG_CONN,
-  IBMS_CLI_MSG_DISCONN,
-  IBMS_CLI_MSG_BIND,
-  IBMS_CLI_MSG_MAD,
-  IBMS_CLI_MSG_CAP,
-  IBMS_CLI_MSG_QUIT,
-  IBMS_CLI_MSG_UNKOWN
+typedef enum _ibms_cli_msg_type {
+    IBMS_CLI_MSG_CONN,
+    IBMS_CLI_MSG_DISCONN,
+    IBMS_CLI_MSG_BIND,
+    IBMS_CLI_MSG_MAD,
+    IBMS_CLI_MSG_CAP,
+    IBMS_CLI_MSG_QUIT,
+    IBMS_CLI_MSG_UNKOWN
 } ibms_cli_msg_type_t;
 /*
 * IBMS_CLI_MSG_CONN
@@ -97,70 +98,31 @@ typedef enum _ibms_cli_msg_type
 *
 *********/
 
-/****s* IBMgtSim: ClientIfc/ibms_mad_addr_t
-* NAME
-*	ibms_mad_addr_t
-*
-* DESCRIPTION
-*	MAD Address
-*
-* SYNOPSIS
-*/
-#include <complib/cl_packon.h>
-typedef struct _ibms_mad_addr
-{
-  uint8_t    sl;
-  int        pkey_index;
-  uint16_t   slid;
-  uint16_t   dlid;
-  uint32_t   sqpn;
-  uint32_t   dqpn;
-} PACK_SUFFIX ibms_mad_addr_t;
-#include <complib/cl_packoff.h>
-/*
-* FIELDS
-*	sl
-*
-*	pkey_index
-*
-*  slid
-*
-*  dlid
-*
-*  sqpn
-*
-*  dqpn
-*
-* SEE ALSO
-*********/
-
-#define MAD_HEADER_SIZE 24
 
 /****s* IBMgtSim: ClientIfc/ibms_conn_msg_t
 * NAME
-*	ibms_conn_msg_t
+*   ibms_conn_msg_t
 *
 * DESCRIPTION
-*	Connecting to the server message
+*   Connecting to the server message
 *
 * SYNOPSIS
 */
 #include <complib/cl_packon.h>
-typedef struct _ib_conn_msg
-{
-  uint8_t   port_num;
-  uint64_t  port_guid;
-  char      host[128];
-  unsigned short int in_msg_port;
+typedef struct _ib_conn_msg {
+    uint8_t   port_num;
+    uint64_t  port_guid;
+    char      host[128];
+    unsigned short int in_msg_port;
 } PACK_SUFFIX ibms_conn_msg_t;
 #include <complib/cl_packoff.h>
 /*
 * FIELDS
-*	port_num
-*		The IB port number the client attaches to.
+*  port_num
+*     The IB port number the client attaches to.
 *
-*	port_guid
-*		The guid of the port to connect to.
+*  port_guid
+*     The guid of the port to connect to.
 *
 *  host
 *     Client host name (null terminated string)
@@ -171,63 +133,104 @@ typedef struct _ib_conn_msg
 * SEE ALSO
 *********/
 
+
 /****s* IBMgtSim: ClientIfc/ibms_disconn_msg_t
 * NAME
-*	ibms_disconn_msg_t
+*   ibms_disconn_msg_t
 *
 * DESCRIPTION
-*	Disconnecting from the server message
+*   Disconnecting from the server message
 *
 * SYNOPSIS
 */
 #include <complib/cl_packon.h>
-typedef struct _ib_disconn_msg
-{
-  uint8_t   port_num;
-  uint64_t  port_guid;
+typedef struct _ib_disconn_msg {
+    uint8_t   port_num;
+    uint64_t  port_guid;
 } PACK_SUFFIX ibms_disconn_msg_t;
 #include <complib/cl_packoff.h>
 /*
 * FIELDS
-*	port_num
-*		The IB port number the client attached to.
+*   port_num
+*     The IB port number the client attached to.
 *
-*	port_guid
-*		The guid of the port to disconnect from.
+*   port_guid
+*     The guid of the port to disconnect from.
 *
 * SEE ALSO
 *********/
 
-/****s* IBMgtSim: ClientIfc/ibms_mad_msg_t
+
+/****s* IBMgtSim: ClientIfc/ibms_mad_addr_t
 * NAME
-*	ibms_mad_msg_t
+*   ibms_mad_addr_t
 *
 * DESCRIPTION
-*	MAD Message
+*   MAD Address
 *
 * SYNOPSIS
 */
 #include <complib/cl_packon.h>
-typedef struct _ibms_mad_msg
-{
-  ibms_mad_addr_t  addr;
-  ib_mad_t         header;
-  uint8_t          payload[MAD_BLOCK_SIZE - MAD_HEADER_SIZE];
+typedef struct _ibms_mad_addr {
+    uint8_t    sl;
+    int        pkey_index;
+    uint16_t   slid;
+    uint16_t   dlid;
+    uint32_t   sqpn;
+    uint32_t   dqpn;
+} PACK_SUFFIX ibms_mad_addr_t;
+#include <complib/cl_packoff.h>
+/*
+* FIELDS
+*   sl
+*
+*   pkey_index
+*
+*   slid
+*
+*   dlid
+*
+*   sqpn
+*
+*   dqpn
+*
+* SEE ALSO
+*********/
+
+
+#define MAD_HEADER_SIZE 24
+
+/****s* IBMgtSim: ClientIfc/ibms_mad_msg_t
+* NAME
+*   ibms_mad_msg_t
+*
+* DESCRIPTION
+*   MAD Message
+*
+* SYNOPSIS
+*/
+#include <complib/cl_packon.h>
+typedef struct _ibms_mad_msg {
+    ibms_mad_addr_t  addr;
+    ib_mad_t         header;
+    uint8_t          payload[MAD_BLOCK_SIZE - MAD_HEADER_SIZE];
 } PACK_SUFFIX ibms_mad_msg_t;
 #include <complib/cl_packoff.h>
 /*
 * FIELDS
-*	mad_hdr
-*		MAD header
+*  mad_hdr
+*     MAD header
 *
-*	payload
-*		MAD data
+*  payload
+*	  MAD data
 *
 *  address
 *     Where do we sent it
 *
 * SEE ALSO
 *********/
+
+
 #define IBMS_BIND_MASK_PORT  1
 #define IBMS_BIND_MASK_QP    2
 #define IBMS_BIND_MASK_CLASS 4
@@ -237,24 +240,23 @@ typedef struct _ibms_mad_msg
 
 /****s* IBMgtSim: ClientIfc/ibms_bind_msg_t
 * NAME
-*	ibms_bind_msg_t
+*   ibms_bind_msg_t
 *
 * DESCRIPTION
-*	Binding Message
+*   Binding Message
 *
 * SYNOPSIS
 */
 #include <complib/cl_packon.h>
-typedef struct _ibms_bind_msg
-{
-  uint8_t      port;
-  uint32_t     qpn;
-  uint8_t      mgt_class;
-  uint8_t      method;
-  uint16_t     attribute;
-  uint8_t      only_input;
-  uint8_t      mask;
-} PACK_SUFFIX ibms_bind_msg_t;
+typedef struct _ibms_bind_msg {
+    uint8_t      port;
+    uint32_t     qpn;
+    uint8_t      mgt_class;
+    uint8_t      method;
+    uint16_t     attribute;
+    uint8_t      only_input;
+    uint8_t      mask;
+    } PACK_SUFFIX ibms_bind_msg_t;
 #include <complib/cl_packoff.h>
 /*
 * FIELDS
@@ -275,22 +277,22 @@ typedef struct _ibms_bind_msg
 * SEE ALSO
 *********/
 
+
 /****s* IBMgtSim: ClientIfc/ibms_cap_msg_t
 * NAME
-*	ibms_cap_msg_t
+*   ibms_cap_msg_t
 *
 * DESCRIPTION
-*	Capability Mask Message = set the port capabilities.
-*  we simulate the VAPI set port capability mask with this API.
+*   Capability Mask Message = set the port capabilities.
+*   we simulate the VAPI set port capability mask with this API.
 *
 * SEE ALSO:
 *   ib_types IB_PORT_CAP_* for the list of capabilities
 */
 #include <complib/cl_packon.h>
-typedef struct _ibms_cap_msg
-{
-  uint32_t     capabilities;
-  uint32_t     mask;
+typedef struct _ibms_cap_msg {
+    uint32_t     capabilities;
+    uint32_t     mask;
 } PACK_SUFFIX ibms_cap_msg_t;
 #include <complib/cl_packoff.h>
 /*
@@ -304,6 +306,7 @@ typedef struct _ibms_cap_msg
 * SEE ALSO
 *********/
 
+
 /****s* IBMgtSim: ClientIfc/ibms_client_msg_t
 * NAME
 *  ibms_client_msg_t
@@ -314,16 +317,15 @@ typedef struct _ibms_cap_msg
 * SYNOPSIS
 */
 #include <complib/cl_packon.h>
-typedef struct _ibms_client_msg
-{
-  ibms_cli_msg_type_t msg_type;
-  union _msg {
-    ibms_mad_msg_t     mad;
-    ibms_bind_msg_t    bind;
-    ibms_cap_msg_t     cap;
-    ibms_conn_msg_t    conn;
-    ibms_disconn_msg_t disc;
-  } PACK_SUFFIX msg;
+typedef struct _ibms_client_msg {
+    ibms_cli_msg_type_t msg_type;
+    union _msg {
+        ibms_mad_msg_t     mad;
+        ibms_bind_msg_t    bind;
+        ibms_cap_msg_t     cap;
+        ibms_conn_msg_t    conn;
+        ibms_disconn_msg_t disc;
+    } PACK_SUFFIX msg;
 } PACK_SUFFIX ibms_client_msg_t;
 #include <complib/cl_packoff.h>
 /*
@@ -339,6 +341,7 @@ typedef struct _ibms_client_msg
 *
 *********/
 
+
 /****s* IBMgtSim: ClientIfc/ibms_response_t
 * NAME
 *  ibms_response_t
@@ -349,9 +352,8 @@ typedef struct _ibms_client_msg
 * SYNOPSIS
 */
 #include <complib/cl_packon.h>
-typedef struct _ibms_response
-{
-  int status;
+typedef struct _ibms_response {
+    int status;
 } PACK_SUFFIX ibms_response_t;
 #include <complib/cl_packoff.h>
 /*
@@ -362,9 +364,10 @@ typedef struct _ibms_response
 *
 *********/
 
+
 #ifdef __cplusplus
 }
 #endif
 
 
-#endif /* IBMGTSIM_CLIENT_IFC */
+#endif          /* IBMGTSIM_CLIENT_IFC */
